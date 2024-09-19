4b2912020d0a1598c2acb312f27bd46f7ee4872f
Abi S George <abisgeorge@Abis-Air.broadband>
2024-09-19 12:42:07 +0100

    explore HTTP methods like get post

--- src/app.js
+++ src/app.js
@@ -4,12 +4,29 @@ const app= express();
 const port= 3000
 
 
-app.use("/home",(req,res)=>{
-   res.send("home")
+app.use("/",(req,res)=>{
+    res.send("no changes") 
+    
 })
-app.use("/about",(req,res)=>{
-   res.send("About")
+
+app.get("/",(req,res)=>{
+    res.send("hello from home page")
+    
 })
+app.post("/",(req,res)=>{
+   console.log("data recived")
+    res.send("data successfully stored")
+    
+})
+app.delete("/",(req,res)=>{
+    res.send("data deleted")
+    
+})
+app.put("/",(req,res)=>{
+    res.send("details updated")
+    
+})
+
 
 
 app.listen(port,()=>{
